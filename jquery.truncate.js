f8f13f027823ab2485f76a8dbcec2c51231aadd7
JeffreyArts <sjeffff@gmail.com>
2015-02-17 09:23:11 +0100

    Divide triggers from main

--- jquery.truncate.js
+++ jquery.truncate.js
@@ -59,13 +59,3 @@ truncateAll = function(){
 		$(this).truncate();
 	});
 }
-
-// Execute truncate on document load
-$(document).ready(function(){
-	truncateAll();
-});
-
-// Execute truncate on window resize
-$( window ).resize(function() { 
-	truncateAll();
-});
